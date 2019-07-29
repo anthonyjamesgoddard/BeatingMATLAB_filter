13c14bb91c0f33da9746c2aec85762e27ba071ff
anthonyjamesgoddard <antg3254@gmail.com>
2019-07-29 11:51:01 +0100

    matches MATLAB ouput - essentially a reverse engineered filter function at this point.

--- decimateComparison.cpp
+++ decimateComparison.cpp
@@ -4,15 +4,16 @@
 constexpr int SIZE = 20;
 constexpr int FILTERSIZE = 5;
 const std::array<float, FILTERSIZE> filter{0.1, 1.1, 2.1, 3.1, 4.1};
-std::array<float, 2*(FILTERSIZE - 1)> bank = {};
+std::array<float, 2 * (FILTERSIZE - 1)> bank = {};
 
-void decimate_init(int8_t* data, int8_t* out) {
+void decimate(int8_t* data, int8_t* out) {
     std::vector<float> output(SIZE);
-    int k = 0;
+    int k;
     // the first chunk of the filter O(FILTERSIZE*FILTERSIZE)
-    for (int i = 0; i < FILTERSIZE; i += 2) {
+    for (int i = 0; i < 2 * FILTERSIZE; i += 2) {
         float temp1 = 0, temp2 = 0;
-        for (int j = 0; j < FILTERSIZE; j++, k = j + 1) {
+        k = 0;
+        for (int j = 0; j < FILTERSIZE; j++, k += 2) {
             if (i - k < 0) break;
             temp1 += filter[j] * data[i - k];
             temp2 += filter[j] * data[i - k + 1];
@@ -20,12 +21,16 @@ void decimate_init(int8_t* data, int8_t* out) {
         output[i] = temp1;
         output[i + 1] = temp2;
     }
+    for (int i = 0; i < 2 * (FILTERSIZE-1); i += 2) {
+        output[i] += bank[i];
+        output[i + 1] += bank[i+1];
+    }
     // this is the AVX-able part
     // the main part O(FILTERSIZE*SIZE)
-    k = 0;
-    for (int i = FILTERSIZE; i < SIZE; i+=2) {
+    for (int i = 2 * FILTERSIZE; i < SIZE; i += 2) {
         float temp1 = 0, temp2 = 0;
-        for (int j = 0; j < FILTERSIZE; j++, k = j + 1) {
+        k = 0;
+        for (int j = 0; j < FILTERSIZE; j++, k += 2) {
             temp1 += filter[j] * data[i - k];
             temp2 += filter[j] * data[i - k + 1];
         }
@@ -33,67 +38,45 @@ void decimate_init(int8_t* data, int8_t* out) {
         output[i + 1] = temp2;
     }
     // update the bank O(FILTERSIZE*FILTERSIZE)
-    k = 0;
-    int offset = SIZE - FILTERSIZE;
-    for (int i = 0; i < 2*(FILTERSIZE - 1); i+=2) {
-        for (int j = 0; j < FILTERSIZE; j++, k = j + 1) {
-            std::cout << i << " "  << offset + i - k << std::endl;
-            bank[i] += filter[j] * data[offset + i - k];
-            bank[i + 1] += filter[j] * data[offset + i - k + 1];
+    for (int i = 0; i < 2 * (FILTERSIZE - 1); i += 2) {
+        k = 0;
+        for (int j = 0; j < FILTERSIZE - 1; j++, k += 2) {
+            if (SIZE - 2 + i - k > SIZE - 1) continue;
+            bank[i] += filter[j + 1] * data[SIZE - 2 + i - k];
+            bank[i + 1] += filter[j + 1] * data[SIZE - 2 + i - k + 1];
         }
     }
     size_t j = 0;
     for (size_t i = 0; i < SIZE; i += 2) {
-        out[j++] = output[i];
-        out[j++] = output[i + 1];
-    }
-}
-
-void decimate(int8_t* data, int8_t* out) {
-    std::vector<float> output(SIZE);
-    int k = 0;
-    for (int i = 0; i < FILTERSIZE; i += 2) {
-        float temp1 = 0, temp2 = 0;
-        for (int j = 0; j < FILTERSIZE; j++, k = j + 1) {
-            if (i - k < 0) break;
-            temp1 += filter[j] * data[i - k];
-            temp2 += filter[j] * data[i - k + 1];
-        }
-        output[i] = temp1;
-        output[i + 1] = temp2;
-    }
-    k = 0;
-    for (int i = FILTERSIZE; i < SIZE; i++) {
-        float temp1 = 0, temp2 = 0;
-        for (int j = 0; j < 5; j++, k = j + 1) {
-            temp1 += filter[j] * data[i - k];
-            temp2 += filter[j] * data[i - k + 1];
-        }
-        output[i] = temp1;
-        output[i + 1] = temp2;
-    }
-    size_t j = 0;
-    for (size_t i = 0; i < SIZE; i += 2) {
-        out[j++] = output[i];
-        out[j++] = output[i + 1];
+        out[j++] = output[i]+0.5;
+        out[j++] = -1 * (output[i + 1]+0.5);
     }
 }
 
 int main() {
     std::cout << "start test" << std::endl;
-    int8_t* data = new int8_t[20];
-    int8_t* out = new int8_t[20];
-    for (int i = 0; i < SIZE; i++) {
-        data[i] = 1;
+    int8_t* data = new int8_t[SIZE];
+    int8_t* out = new int8_t[SIZE];
+    int j = 0;
+    for (int i = 0; i < SIZE / 2; i++) {
+        data[j++] = i + 1;
+        data[j++] = i + 1;
     }
-    decimate_init(data, out);
+    decimate(data, out);
     std::cout << "bank after decimation" << std::endl;
-    for(int i = 0; i < 2*(FILTERSIZE - 1); i+=2) {
-        std::cout << bank[i] << " " << bank[i+1] << std::endl;
+    for (int i = 0; i < 2 * (FILTERSIZE - 1); i += 2) {
+        std::cout << bank[i] << " " << bank[i + 1] << std::endl;
     }
     std::cout << "output" << std::endl;
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << static_cast<int>(out[i]) << std::endl;
+    for (int i = 0; i < SIZE; i += 2) {
+        std::cout << static_cast<int>(out[i]) << " "
+                  << static_cast<int>(out[i + 1]) << std::endl;
+    }
+    std::cout << "now we need to rerun the decimation" << std::endl;
+    decimate(data, out);
+    for (int i = 0; i < SIZE; i += 2) {
+        std::cout << static_cast<int>(out[i]) << " "
+                  << static_cast<int>(out[i + 1]) << std::endl;
     }
 }
 
