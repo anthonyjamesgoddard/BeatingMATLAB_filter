c28c06a7d137a3a7c39da9e634fa8c9eb80b8856
anthonyjamesgoddard <antg3254@gmail.com>
2019-08-05 10:37:42 +0100

    adding changes for blog post

--- decimateComparison.cpp
+++ decimateComparison.cpp
@@ -5,14 +5,23 @@
 #include <iostream>
 #include <vector>
 
-constexpr int SIZE = 2000000;
+constexpr int SIZE = 200000;
 constexpr int FILTERSIZE = 10;
 const std::array<float, FILTERSIZE> filter{0.1, 1.1, 2.1, 3.1, 4.1,
                                            0.1, 1.1, 2.1, 3.1, 4.1};
 std::array<float, FILTERSIZE> filter_op = filter;
 std::array<float, 2 * (FILTERSIZE - 1)> bank = {};
+std::array<float, 2 * (FILTERSIZE - 1)> bank_r = {};
 std::array<float, (FILTERSIZE - 1)> bankr = {};
 std::array<float, (FILTERSIZE - 1)> bankc = {};
+
+#define EXECUTE(j)                                           \
+    {                                                        \
+        __m256 floats1 = _mm256_load_ps(&datar[b + j]);      \
+        __m256 floats2 = _mm256_load_ps(&datac[b + j]);      \
+        res1 = _mm256_fmadd_ps(revKernel[j], floats1, res1); \
+        res2 = _mm256_fmadd_ps(revKernel[j], floats2, res2); \
+    }
 
 namespace {
 __m256 revKernel[FILTERSIZE];
@@ -57,13 +66,91 @@ void FIR(float* data, float* output) {
     }
 }
 
-/*
- * 1)  Convert the problem into a cross-correlation
- *     (basicallly dot products) vs a convolution
- * 2)  AVX-ify it!
- */
+void FIR_rev(float* data, float* output) {
+    int k, b;
+    for (int i = 0; i < 2 * FILTERSIZE; i += 2) {
+        float temp1 = 0, temp2 = 0;
+        k = 0;
+        b = i - 2 * FILTERSIZE + 2;
+        for (int j = 0; j < FILTERSIZE; j++, k += 2) {
+            if (b + k < 0) continue;
+            temp1 += filter_op[j] * data[b + k];
+            temp2 += filter_op[j] * data[b + k + 1];
+        }
+        output[i] = temp1;
+        output[i + 1] = temp2;
+    }
+    for (int i = 0; i < 2 * (FILTERSIZE - 1); i += 2) {
+        output[i] += bank_r[i];
+        output[i + 1] += bank_r[i + 1];
+    }
+    for (int i = 2 * FILTERSIZE; i < SIZE; i += 2) {
+        float temp1 = 0, temp2 = 0;
+        k = 0;
+        b = i - 2 * FILTERSIZE + 2;
+        for (int j = 0; j < FILTERSIZE; j++, k += 2) {
+            temp1 += filter_op[j] * data[b + k];
+            temp2 += filter_op[j] * data[b + k + 1];
+        }
+        output[i] = temp1;
+        output[i + 1] = temp2;
+    }
+    for (int i = 0; i < 2 * (FILTERSIZE - 1); i += 2) {
+        k = 0;
+        b = i - 2 * FILTERSIZE + 2;
+        for (int j = 0; j < FILTERSIZE - 1; j++, k += 2) {
+            if (SIZE + b + k > SIZE - 1) break;
+            int idx = SIZE + b + k;
+            bank_r[i] += filter_op[j] * data[idx];
+            bank_r[i + 1] += filter_op[j] * data[idx + 1];
+        }
+    }
+}
 
 void FIR_optim(float* datar, float* datac, float* outputr, float* outputc) {
+    int b;
+    float temp1, temp2;
+    for (int i = 0; i < FILTERSIZE; i++) {
+        temp1 = 0;
+        temp2 = 0;
+        b = i - FILTERSIZE + 1;
+        for (int j = 0; j < FILTERSIZE; j++) {
+            if (b + j < 0) continue;
+            temp1 += filter_op[j] * datar[b + j];
+            temp2 += filter_op[j] * datac[b + j];
+        }
+        outputr[i] = temp1;
+        outputc[i] = temp2;
+    }
+    for (int i = 0; i < (FILTERSIZE - 1); i++) {
+        outputr[i] += bankr[i];
+        outputc[i] += bankc[i];
+    }
+    for (int i = FILTERSIZE; i < SIZE / 2; i++) {
+        b = i - FILTERSIZE + 1;
+        temp1 = 0;
+        temp2 = 0;
+        for (int j = 0; j < FILTERSIZE; j++) {
+            if (b + j < 0) continue;
+            temp1 += filter_op[j] * datar[b + j];
+            temp2 += filter_op[j] * datac[b + j];
+        }
+        outputr[i] = temp1;
+        outputc[i] = temp2;
+    }
+    int idx;
+    for (int i = 0; i < (FILTERSIZE - 1); i++) {
+        b = i - FILTERSIZE + 1;
+        for (int j = 0; j < FILTERSIZE - 1; j++) {
+            idx = SIZE / 2 + b + j;
+            if (idx > SIZE / 2 - 1) break;
+            bankr[i] += filter_op[j] * datar[idx];
+            bankc[i] += filter_op[j] * datac[idx];
+        }
+    }
+}
+
+void FIR_optim_avx(float* datar, float* datac, float* outputr, float* outputc) {
     int b;
     for (int i = 0; i < FILTERSIZE; i++) {
         float temp1 = 0, temp2 = 0;
@@ -85,12 +172,16 @@ void FIR_optim(float* datar, float* datac, float* outputr, float* outputc) {
         b = i - FILTERSIZE + 1;
         __m256 res1 = _mm256_setzero_ps();
         __m256 res2 = _mm256_setzero_ps();
-        for (int j = 0; j < FILTERSIZE; j++) {
-            __m256 floats1 = _mm256_load_ps(&datar[b + j]);
-            __m256 floats2 = _mm256_load_ps(&datac[b + j]);
-            res1 = _mm256_fmadd_ps(revKernel[j], floats1, res1);
-            res2 = _mm256_fmadd_ps(revKernel[j], floats2, res2);
-        }
+        EXECUTE(0);
+        EXECUTE(1);
+        EXECUTE(2);
+        EXECUTE(3);
+        EXECUTE(4);
+        EXECUTE(5);
+        EXECUTE(6);
+        EXECUTE(7);
+        EXECUTE(8);
+        EXECUTE(9);
         _mm256_storeu_ps(&outputr[i], res1);
         _mm256_storeu_ps(&outputc[i], res2);
     }
@@ -119,15 +210,24 @@ void FIR_optim(float* datar, float* datac, float* outputr, float* outputc) {
 int main() {
     std::cout << "start test" << std::endl;
     float* data = new float[SIZE];
+    float* data_r = new float[SIZE];
+    float* datasplitr = new float[SIZE / 2];
+    float* datasplitc = new float[SIZE / 2];
     float* datar = new float[SIZE / 2];
     float* datac = new float[SIZE / 2];
     float* out = new float[SIZE];
+    float* out_r = new float[SIZE];
     float* outOptimised = new float[SIZE];
+    float* outSplitr = new float[SIZE / 2];
+    float* outSplitc = new float[SIZE / 2];
     float* outOptimisedr = new float[SIZE / 2];
     float* outOptimisedc = new float[SIZE / 2];
+
     for (int i = 0; i < SIZE; i++) {
         data[i] = i + 1;
+        data_r[i] = i + 1;
     }
+
     int j = 0;
     for (int i = 0; i < SIZE / 2; i++) {
         datar[i] = data[j++];
@@ -139,19 +239,19 @@ int main() {
     std::reverse(filter_op.data(), filter_op.data() + FILTERSIZE);
     for (size_t i = 0; i < FILTERSIZE; i++)
         revKernel[i] = _mm256_set1_ps(filter[FILTERSIZE - i - 1]);
+    FIR_rev(data_r, out_r);
     FIR_optim(datar, datac, outOptimisedr, outOptimisedc);
-
-    std::cout << "output" << std::endl;
+    FIR_optim_avx(datasplitr, datasplitc, outSplitr, outSplitc);
+    //   std::cout << "output" << std::endl;
 
     j = 0;
+    /*
     for (int i = 0; i < SIZE / 2; i++) {
-        /*
         std::cout << i << std::endl;
         std::cout << "normal: " << out[j] << " " << out[j + 1] << std::endl;
         std::cout << "optmised: " << outOptimisedr[i] << " " << outOptimisedc[i]
                   << std::endl;
         std::cout << std::endl;
-
         if (out[j] != outOptimisedr[i]) {
             std::cout << i << std::endl;
             std::cout << "difference " << out[j] - outOptimisedr[i]
@@ -163,10 +263,10 @@ int main() {
                       << std::endl;
         }
         j += 2;
-        */
     }
     std::cout << std::endl;
-
+*/
+    /*
     std::cout << " bank for normal " << std::endl;
     for (int i = 0; i < 2 * (FILTERSIZE - 1); i += 2) {
         // std::cout << bank[i] << " " << bank[i + 1] << std::endl;
@@ -175,19 +275,49 @@ int main() {
     for (int i = 0; i < (FILTERSIZE - 1); i++) {
         // std::cout << bankr[i] << " " << bankc[i] << std::endl;
     }
-
-    std::cout << "now we need to rerun the decimation" << std::endl;
-
+*/
+    // std::cout << "now we need to rerun the decimation" << std::endl;
+    // TIMING ---------------
     std::chrono::steady_clock::time_point begin =
         std::chrono::steady_clock::now();
     std::cout << "optimised version" << std::endl;
 
     for (int i = 0; i < 1000; i++) {
-        FIR_optim(datar, datac, outOptimisedr, outOptimisedc);
+        FIR_optim_avx(datasplitr, datasplitc, outSplitr, outSplitc);
     }
 
     std::chrono::steady_clock::time_point end =
         std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                     .count()
+              << "[ms]" << std::endl;
+    // -----------------------
+    begin = std::chrono::steady_clock::now();
+    std::cout << "vanilla-reverse version" << std::endl;
+
+    for (int i = 0; i < 1000; i++) {
+        FIR_optim(datar, datac, outOptimisedr, outOptimisedc);
+    }
+
+    end = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                     .count()
+              << "[ms]" << std::endl;
+    // -----------------------
+    begin = std::chrono::steady_clock::now();
+    std::cout << "vanilla-reverse version" << std::endl;
+
+    for (int i = 0; i < 1000; i++) {
+        FIR_rev(data_r, out_r);
+    }
+
+    end = std::chrono::steady_clock::now();
 
     std::cout << "Time difference = "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end -
