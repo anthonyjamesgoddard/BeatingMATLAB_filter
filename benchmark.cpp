a19d2507c397b48fc5b9338db53464a6c65b4bcb
llyr-who <jamesbushnell1992@gmail.com>
2020-05-26 19:38:35 +0100

    benchmarking complete

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,10 +5,27 @@
 #include <iostream>
 #include <vector>
 
-constexpr int SIZE = 200000;
-constexpr int FILTERSIZE = 10;
-const std::array<float, FILTERSIZE> filter{0.1, 1.1, 2.1, 3.1, 4.1,
-                                           0.1, 1.1, 2.1, 3.1, 4.1};
+void FIR(float* data, float* output);
+void FIR_rev(float* data, float* output);
+void FIR_optim(float* datar, float* datac, float* outputr, float* outputc);
+void FIR_optim_avx(float* datar, float* datac, float* outputr, float* outputc);
+
+constexpr int SIZE = 800000;
+constexpr int FILTERSIZE = 8;
+
+template <std::size_t... I>
+constexpr std::array<float, sizeof...(I)> fillArray(std::index_sequence<I...>) {
+    return std::array<float, sizeof...(I)>{float(I + 5) /
+                                           float(sizeof...(I))...};
+}
+
+template <std::size_t N>
+constexpr std::array<float, N> fillArray() {
+    return fillArray(std::make_index_sequence<N>{});
+}
+
+const std::array<float, FILTERSIZE> filter = fillArray<FILTERSIZE>();
+
 std::array<float, FILTERSIZE> filter_op = filter;
 std::array<float, 2 * (FILTERSIZE - 1)> bank = {};
 std::array<float, 2 * (FILTERSIZE - 1)> bank_r = {};
@@ -25,6 +42,95 @@ std::array<float, (FILTERSIZE - 1)> bankc = {};
 
 namespace {
 __m256 revKernel[FILTERSIZE];
+}
+
+int main() {
+    float* data = new float[SIZE];
+    float* data_r = new float[SIZE];
+    float* datasplitr = new float[SIZE / 2];
+    float* datasplitc = new float[SIZE / 2];
+    float* datar = new float[SIZE / 2];
+    float* datac = new float[SIZE / 2];
+    float* out = new float[SIZE];
+    float* out_r = new float[SIZE];
+    float* outOptimised = new float[SIZE];
+    float* outSplitr = new float[SIZE / 2];
+    float* outSplitc = new float[SIZE / 2];
+    float* outOptimisedr = new float[SIZE / 2];
+    float* outOptimisedc = new float[SIZE / 2];
+
+    // set data;
+    for (int i = 0; i < SIZE; i++) {
+        data[i] = i + 1;
+        data_r[i] = SIZE - i;
+    }
+
+    int j = 0;
+    for (int i = 0; i < SIZE / 2; i++) {
+        datar[i] = data[j++];
+        datac[i] = data[j++];
+    }
+    // reverse filter first;
+    std::reverse(filter_op.data(), filter_op.data() + FILTERSIZE);
+    for (size_t i = 0; i < FILTERSIZE; i++)
+        revKernel[i] = _mm256_set1_ps(filter[FILTERSIZE - i - 1]);
+
+    std::chrono::steady_clock::time_point begin =
+        std::chrono::steady_clock::now();
+
+    for (int i = 0; i < 1000; i++) {
+        FIR_optim_avx(datasplitr, datasplitc, outSplitr, outSplitc);
+    }
+
+    std::cout << FILTERSIZE << " " << SIZE << " ";
+
+    std::chrono::steady_clock::time_point end =
+        std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                     .count()
+              << " ";
+    // -----------------------
+    begin = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < 1000; i++) {
+        FIR_optim(datar, datac, outOptimisedr, outOptimisedc);
+    }
+
+    end = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                     .count()
+              << " ";
+    // -----------------------
+    begin = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < 1000; i++) {
+        FIR_rev(data_r, out_r);
+    }
+
+    end = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                     .count()
+              << " ";
+
+    begin = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < 1000; i++) {
+        FIR(data, out);
+    }
+
+    end = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                     .count()
+              << std::endl;
+    return 0;
 }
 
 void FIR(float* data, float* output) {
@@ -172,20 +278,13 @@ void FIR_optim_avx(float* datar, float* datac, float* outputr, float* outputc) {
         b = i - FILTERSIZE + 1;
         __m256 res1 = _mm256_setzero_ps();
         __m256 res2 = _mm256_setzero_ps();
-        EXECUTE(0);
-        EXECUTE(1);
-        EXECUTE(2);
-        EXECUTE(3);
-        EXECUTE(4);
-        EXECUTE(5);
-        EXECUTE(6);
-        EXECUTE(7);
-        EXECUTE(8);
-        EXECUTE(9);
+        for (int k = 0; k < FILTERSIZE; ++k) {
+            EXECUTE(k);
+        }
         _mm256_storeu_ps(&outputr[i], res1);
         _mm256_storeu_ps(&outputc[i], res2);
     }
-    for (; i < SIZE / 2; i++) {
+    for (; i < SIZE / 2; ++i) {
         b = i - FILTERSIZE + 1;
         float temp1 = 0, temp2 = 0;
         for (int j = 0; j < FILTERSIZE; j++) {
@@ -205,162 +304,5 @@ void FIR_optim_avx(float* datar, float* datac, float* outputr, float* outputc) {
             bankc[i] += filter_op[j] * datac[idx];
         }
     }
-}
-
-int main() {
-    std::cout << "start test" << std::endl;
-    float* data = new float[SIZE];
-    float* data_r = new float[SIZE];
-    float* datasplitr = new float[SIZE / 2];
-    float* datasplitc = new float[SIZE / 2];
-    float* datar = new float[SIZE / 2];
-    float* datac = new float[SIZE / 2];
-    float* out = new float[SIZE];
-    float* out_r = new float[SIZE];
-    float* outOptimised = new float[SIZE];
-    float* outSplitr = new float[SIZE / 2];
-    float* outSplitc = new float[SIZE / 2];
-    float* outOptimisedr = new float[SIZE / 2];
-    float* outOptimisedc = new float[SIZE / 2];
-
-    for (int i = 0; i < SIZE; i++) {
-        data[i] = i + 1;
-        data_r[i] = SIZE - i;
-    }
-
-    int j = 0;
-    for (int i = 0; i < SIZE / 2; i++) {
-        datar[i] = data[j++];
-        datac[i] = data[j++];
-    }
-    FIR(data, out);
-    // reverse filter first;
-    std::cout << std::endl;
-    std::reverse(filter_op.data(), filter_op.data() + FILTERSIZE);
-    for (size_t i = 0; i < FILTERSIZE; i++)
-        revKernel[i] = _mm256_set1_ps(filter[FILTERSIZE - i - 1]);
-    FIR_rev(data_r, out_r);
-    FIR_optim(datar, datac, outOptimisedr, outOptimisedc);
-    FIR_optim_avx(datasplitr, datasplitc, outSplitr, outSplitc);
-    //   std::cout << "output" << std::endl;
-
-    j = 0;
-    /*
-    for (int i = 0; i < SIZE / 2; i++) {
-        std::cout << i << std::endl;
-        std::cout << "normal: " << out[j] << " " << out[j + 1] << std::endl;
-        std::cout << "optmised: " << outOptimisedr[i] << " " << outOptimisedc[i]
-                  << std::endl;
-        std::cout << std::endl;
-        if (out[j] != outOptimisedr[i]) {
-            std::cout << i << std::endl;
-            std::cout << "difference " << out[j] - outOptimisedr[i]
-                      << std::endl;
-        }
-        if (out[j + 1] != outOptimisedc[i]) {
-            std::cout << i << std::endl;
-            std::cout << "difference " << out[j] - outOptimisedr[i]
-                      << std::endl;
-        }
-        j += 2;
-    }
-    std::cout << std::endl;
-*/
-    /*
-    std::cout << " bank for normal " << std::endl;
-    for (int i = 0; i < 2 * (FILTERSIZE - 1); i += 2) {
-        // std::cout << bank[i] << " " << bank[i + 1] << std::endl;
-    }
-    std::cout << " bank for optmised " << std::endl;
-    for (int i = 0; i < (FILTERSIZE - 1); i++) {
-        // std::cout << bankr[i] << " " << bankc[i] << std::endl;
-    }
-*/
-    // std::cout << "now we need to rerun the decimation" << std::endl;
-    // TIMING ---------------
-    std::chrono::steady_clock::time_point begin =
-        std::chrono::steady_clock::now();
-    std::cout << "avx version" << std::endl;
-
-    for (int i = 0; i < 1000; i++) {
-        FIR_optim_avx(datasplitr, datasplitc, outSplitr, outSplitc);
-    }
-
-    std::chrono::steady_clock::time_point end =
-        std::chrono::steady_clock::now();
-
-    std::cout << "Time difference = "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                       begin)
-                     .count()
-              << "[ms]" << std::endl;
-    // -----------------------
-    begin = std::chrono::steady_clock::now();
-    std::cout << "vanilla-reverse real and imaginary version" << std::endl;
-
-    for (int i = 0; i < 1000; i++) {
-        FIR_optim(datar, datac, outOptimisedr, outOptimisedc);
-    }
-
-    end = std::chrono::steady_clock::now();
-
-    std::cout << "Time difference = "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                       begin)
-                     .count()
-              << "[ms]" << std::endl;
-    // -----------------------
-    begin = std::chrono::steady_clock::now();
-    std::cout << "vanilla-reverse version" << std::endl;
-
-    for (int i = 0; i < 1000; i++) {
-        FIR_rev(data_r, out_r);
-    }
-
-    end = std::chrono::steady_clock::now();
-
-    std::cout << "Time difference = "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                       begin)
-                     .count()
-              << "[ms]" << std::endl;
-
-    begin = std::chrono::steady_clock::now();
-    std::cout << "vanilla version" << std::endl;
-
-    for (int i = 0; i < 1000; i++) {
-        FIR(data, out);
-    }
-
-    end = std::chrono::steady_clock::now();
-
-    std::cout << "Time difference = "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                       begin)
-                     .count()
-              << "[ms]" << std::endl;
-    j = 0;
-    for (int i = 0; i < SIZE / 2; i++) {
-        /*
-        std::cout << i << std::endl;
-        std::cout << "normal: " << out[j] << " " << out[j + 1] << std::endl;
-        std::cout << "optmised: " << outOptimisedr[i] << " " << outOptimisedc[i]
-                  << std::endl;
-        std::cout << std::endl;
-
-        if (out[j] != outOptimisedr[i]) {
-            std::cout << i << std::endl;
-            std::cout << "difference " << out[j] - outOptimisedr[i]
-                      << std::endl;
-        }
-        if (out[j + 1] != outOptimisedc[i]) {
-            std::cout << i << std::endl;
-            std::cout << "difference " << out[j] - outOptimisedr[i]
-                      << std::endl;
-        }
-        j += 2;
-        */
-    }
-	return 0;
 }
 
