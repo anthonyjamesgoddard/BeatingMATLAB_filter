85586fd47f1bbe5e5cb3d57cb466f45036b125b4
llyr-who <jamesbushnell1992@gmail.com>
2020-05-27 15:32:02 +0100

    add plots

--- get_figs.py
+++ get_figs.py
@@ -5,7 +5,7 @@ chunk_sizes = [800,8000,80000,800000]
 
 def plot_chunk_data(chunk_size, lines):
     plt.figure()
-    plt.title(str(chunk_size))
+    plt.title("SIZE :" + str(chunk_size))
     x = [line.split()[0] for line in lines if int(line.split()[1]) == chunk_size]
     y1 = [int(line.split()[2]) for line in lines if int(line.split()[1]) == chunk_size]
     y2 = [int(line.split()[3]) for line in lines if int(line.split()[1]) == chunk_size]
@@ -16,11 +16,21 @@ def plot_chunk_data(chunk_size, lines):
     plt.plot(x,y3, label='rev-filter')
     plt.plot(x,y4, label='vanilla')
     plt.legend()
+    plt.savefig(str(chunk_size) + ".pdf")
+
+def plot_avx(lines):
     plt.figure()
+    plt.title("avx")
+    x = [int(line.split()[0]) for line in lines]
+    all_data = [int(line.split()[2]) for line in lines]
+
+    plt.plot(x,all_data, label='avx')
+    plt.show()
+    
 
 with open("linux_benchmarks.txt") as f:
     lines = f.readlines()
     for c in chunk_sizes:
         plot_chunk_data(c, lines)
-    plt.show()
+    plot_avx(lines)
 
